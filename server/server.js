b78dbfb88652d499c8fe789097967a4d4dd2bdb8
tyler <tyler.tongseng@gmail.com>
2023-02-27 18:48:24 +0800

    comments

--- server/server.js
+++ server/server.js
@@ -5,14 +5,12 @@ const cors = require("cors");
 require("dotenv").config();
 const apiKey = process.env.OPENAI_API;
 
+//Import db schema from models
 const db = require("./models");
 
+//Run DB models before running app
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
     console.log("Server running on port 30001");
   });
 });
-// //server running on localhost 3001
-// app.listen(3001, () => {
-//   console.log("Server running on port 30001");
-// });
