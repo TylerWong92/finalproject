d35999cd6e7e295ac5dd84568a4225ffd6b6fb5d
tyler <tyler.tongseng@gmail.com>
2023-02-27 18:45:42 +0800

    Connect db -post To MySQL

--- server/server.js
+++ server/server.js
@@ -5,7 +5,14 @@ const cors = require("cors");
 require("dotenv").config();
 const apiKey = process.env.OPENAI_API;
 
-//server running on localhost 3001
-app.listen(3001, () => {
-  console.log("Server running on port 30001");
+const db = require("./models");
+
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Server running on port 30001");
+  });
 });
+// //server running on localhost 3001
+// app.listen(3001, () => {
+//   console.log("Server running on port 30001");
+// });
