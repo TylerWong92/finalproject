c3f2bd7a49298d491b63d66536c23ca229bff293
tyler <tyler.tongseng@gmail.com>
2023-02-27 18:26:32 +0800

    install sequelize - cli

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-
-const apiKey = process.env.OPENAI_API;
-
-// Use the cors middleware
-
 const app = express();
 
-app.listen(3000);
+const cors = require("cors");
+require("dotenv").config();
+const apiKey = process.env.OPENAI_API;
+
+//server running on localhost 3001
+app.listen(3001, () => {
+  console.log("Server running on port 30001");
+});
