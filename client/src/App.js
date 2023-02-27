4619c8ed3c72c2cf92d2a7ec5c0ccc3a309340d6
tyler <tyler.tongseng@gmail.com>
2023-02-27 21:58:25 +0800

    map post data on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
 import "./App.css";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [listOfPosts, setListOfPosts] = useState([]);
+
   useEffect(() => {
     axios.get("http://localhost:3001/posts").then((response) => {
-      console.log(response.data);
+      setListOfPosts(response.data);
     });
   }, []);
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">hi test test</h1>
+      <h1 className="text-3xl font-bold underline">Final Project</h1>
+
+      {listOfPosts.map((value, key) => {
+        return (
+          <div>
+            <div>{value.title}</div>
+            <div>{value.postText}</div>
+            <div>{value.username}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
