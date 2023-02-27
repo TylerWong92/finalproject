3117a7f53f75bd6ab2fa256bdc3d906bd0d87c82
tyler <tyler.tongseng@gmail.com>
2023-02-27 20:35:47 +0800

    router.post -test

---
+++ server/routes/Post.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+// Use express router
+const router = express.Router();
+const { Posts } = require("../models");
+
+router.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+router.post("/", async (req, res) => {
+  const post = req.body;
+  await Posts.create(post);
+  res.json(post);
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 const cors = require("cors");
 require("dotenv").config();
@@ -7,6 +8,10 @@ const apiKey = process.env.OPENAI_API;
 
 //Import db schema from models
 const db = require("./models");
+
+// Routers
+const postRouter = require("./routes/Post");
+app.use("/posts", postRouter);
 
 //Run DB models before running app
 db.sequelize.sync().then(() => {
