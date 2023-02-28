43cfeac31454c439df38e4605e6a384c4ee55483
tyler <tyler.tongseng@gmail.com>
2023-02-28 17:11:15 +0800

    useParams for post page id

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
+import Post from "./pages/Post";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/createpost" element={<CreatePost />} />
+          <Route path="/post/:id" element={<Post />} />
         </Routes>
       </Router>
     </div>
