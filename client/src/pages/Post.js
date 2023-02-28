b100dfa0bad7572ab5bcee9ac7f4bc7ec8e5bf36
tyler <tyler.tongseng@gmail.com>
2023-02-28 17:41:51 +0800

    Setup Post inner endroute

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Post = () => {
   let { id } = useParams();
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      console.log(response.data);
+    });
+  }, []);
+
   return <div>post{id}</div>;
 };
 
