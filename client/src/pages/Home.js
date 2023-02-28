520c0d26ca821b4f6b859f56a714398635beb1b3
tyler <tyler.tongseng@gmail.com>
2023-02-28 17:39:13 +0800

    Route Post to Post inner page using ID

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const [listOfPosts, setListOfPosts] = useState([]);
+
+  let navigate = useNavigate();
 
   useEffect(() => {
     axios.get("http://localhost:3001/posts").then((response) => {
@@ -15,7 +18,11 @@ const Home = () => {
     <div>
       {listOfPosts.map((value, key) => {
         return (
-          <div>
+          <div
+            onClick={() => {
+              navigate(`/post/${value.id}`);
+            }}
+          >
             <div>{value.title}</div>
             <div>{value.postText}</div>
             <div>{value.username}</div>
