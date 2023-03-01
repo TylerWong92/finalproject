4006084023aff22307a69c23650769ce48f1438f
tyler <tyler.tongseng@gmail.com>
2023-03-01 12:32:24 +0800

    Form Login Registration frontend

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
 const Registration = () => {
-  return <div>Registration Page</div>;
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().min(3).max(15).required(),
+    password: Yup.string().min(4).max(20).required(),
+  });
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/auth", data).then(() => {
+      console.log(data);
+    });
+  };
+
+  return (
+    <div>
+      Registration Page
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form>
+          <legend>Username:</legend>
+          <ErrorMessage name="username" component="span" />
+          <Field name="username" placeholder="(Ex.john)" />
+          <legend>Password:</legend>
+          <ErrorMessage name="password" component="span" />
+          <Field type="password" name="password" placeholder="(Ex.1234)" />
+          <button type="submit">Register Now!</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default Registration;
