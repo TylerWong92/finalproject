6948a51c6b105f64fe8313aa0dc6d25d46377de1
tyler <tyler.tongseng@gmail.com>
2023-03-01 11:59:27 +0800

    Adding App.js routes and links - login -registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Link to="/">Home </Link>
+        <Link to="/createpost">create a post </Link>
+        <Link to="/login">login </Link>
+        <Link to="/registration">registrations </Link>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/createpost" element={<CreatePost />} />
           <Route path="/post/:id" element={<Post />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
