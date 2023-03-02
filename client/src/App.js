f35826292974166413593282e872a3978fdd18c4
tyler <tyler.tongseng@gmail.com>
2023-03-02 18:14:00 +0800

    Create useContext for Auth and useState for Auth

--- client/src/App.js
+++ client/src/App.js
@@ -5,29 +5,40 @@ import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
+import { AuthContext } from "./helpers/AuthContext";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [authState, setAuthState] = useState(false);
+
+  // When page is refresh it would setState to true if accessToken
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      setAuthState(true);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <Router>
-        <Link to="/">Home </Link>
-        <Link to="/createpost">create a post </Link>
-        <Link to="/login">login </Link>
-        <Link to="/registration">registrations </Link>
-        {!localStorage.getItem("accessToken") && (
-          <div>
-            <Link to="/login">login </Link>
-            <Link to="/registration">registrations </Link>
-          </div>
-        )}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/createpost" element={<CreatePost />} />
-          <Route path="/post/:id" element={<Post />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
+      <AuthContext.Provider value={{ authState, setAuthState }}>
+        <Router>
+          <Link to="/">Home </Link>
+          <Link to="/createpost">create a post </Link>
+          {!authState && (
+            <div>
+              <Link to="/login">login </Link>
+              <Link to="/registration">registrations </Link>
+            </div>
+          )}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/createpost" element={<CreatePost />} />
+            <Route path="/post/:id" element={<Post />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      </AuthContext.Provider>
     </div>
   );
 }
