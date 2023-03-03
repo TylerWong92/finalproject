a8f77fb21d14f3b68f523daa2a62b4339fbcabf6
tyler <tyler.tongseng@gmail.com>
2023-03-03 18:06:10 +0800

    Render User post in profile pages

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,21 +1,42 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Profile = () => {
   let { id } = useParams();
+  let navigate = useNavigate();
   const [username, setUsername] = useState("");
+  const [listOfPosts, setListOfPosts] = useState([]);
 
   useEffect(() => {
     axios.get(`http://localhost:3001/auth/basicinfo/${id}`).then((response) => {
       setUsername(response.data.username);
     });
+    axios.get(`http://localhost:3001/posts/byuserId/${id}`).then((response) => {
+      setListOfPosts(response.data);
+    });
   }, []);
+
   return (
     <div>
       profile page
       <h1>username : {username}</h1>
-      <div>list of post</div>
+      <div>
+        {listOfPosts.map((value, key) => {
+          return (
+            <div
+              key={key}
+              onClick={() => {
+                navigate(`/post/${value.id}`);
+              }}
+            >
+              <div>{value.title}</div>
+              <div>{value.postText}</div>
+              <div>{value.username}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
